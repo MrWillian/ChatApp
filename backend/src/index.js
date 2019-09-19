3800c5261088d0b01d2373aa6961351813a4ecdc
MrWillian <willian.dev10@gmail.com>
2019-09-19 14:44:49 -0300

    Ajustes na emiçao da mensagem de chat

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 io.on('connection', socket => {
     console.log('a user connected...');
+    socket.on('chat message', message => {
+        console.log(message);
+        io.emit('chat message', message);
+    });
 });
 
 server.listen(port, () => console.log('connection is open on port: ' + port));
