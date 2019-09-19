8788802765a8e5a20a7dbf4c298da60a842de9a0
MrWillian <willian.dev10@gmail.com>
2019-09-19 15:39:13 -0300

    Adiçao do Mongoose e Cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
+const mongoose = require('mongoose');
+const cors = require('cors');
 const port = 3000;
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0-hn3qw.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
 
 io.on('connection', socket => {
     console.log('a user connected...');
@@ -11,5 +17,8 @@ io.on('connection', socket => {
         io.emit('chat message', message);
     });
 });
+
+app.use(cors());
+app.use(express.json());
 
 server.listen(port, () => console.log('connection is open on port: ' + port));
